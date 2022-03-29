2f3b6754168e06fe012261d9b5f765aa3506afb3
shivam.jha <shivam.jha@trell.in>
2022-03-30 01:56:28 +0530

    adding error handler

--- controllers/GET/getUserDetails.go
+++ controllers/GET/getUserDetails.go
@@ -5,10 +5,14 @@ import (
 
 	"github.com/gin-gonic/gin"
 	requestStruct "github.com/shivamsouravjha/Micro-Game/struct/request"
+	"github.com/shivamsouravjha/Micro-Game/utils"
 )
 
 func GetUserDetails(c *gin.Context) {
 	getCreatorRequest := requestStruct.GetUserDetailsRequest{}
-
+	if err := c.ShouldBind(&getCreatorRequest); err != nil {
+		c.JSON(422, utils.SendErrorResponse(err))
+		return
+	}
 	c.JSON(http.StatusOK, getCreatorRequest)
 }

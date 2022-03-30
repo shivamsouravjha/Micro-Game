29b4e05cf78575e53e3aca6a2ac69231f068f695
shivam.jha <shivam.jha@trell.in>
2022-03-30 09:54:45 +0530

    revamping structure

--- UserService/helpers/db/createUser.go
+++ UserService/helpers/db/createUser.go
@@ -11,16 +11,19 @@ import (
 
 func CreateUser(ctx context.Context, createUser *requestStruct.CreateUserDetails) string {
 	var alreadyExisingUser []structs.UserDetails
+
 	sqlString := fmt.Sprintf("SELECT userId,firstName,lastName,penName,userEmail,bio,number FROM `user` WHERE `penName` =  \"%v\"", createUser.PenName)
 	_, err := structss.Dbmap.Select(&alreadyExisingUser, sqlString)
 	if len(alreadyExisingUser) != 0 || err != nil {
 		return "Penname already exists"
 	}
+
 	sqlString = fmt.Sprintf("SELECT userId,firstName,lastName,penName,userEmail,bio,number FROM `user` WHERE `userEmail` = \"%v\"", createUser.UserEmail)
 	_, err = structss.Dbmap.Select(&alreadyExisingUser, sqlString)
 	if len(alreadyExisingUser) != 0 || err != nil {
 		return "Email taken"
 	}
+
 	_, err = structss.Dbmap.Exec("INSERT INTO user (firstName,lastName,penName,userEmail,bio,number) VALUES(?,?,?,?,?,?)", createUser.FirstName, createUser.LastName, createUser.PenName, createUser.UserEmail, createUser.Bio, createUser.Number)
 	if err != nil {
 		return err.Error()

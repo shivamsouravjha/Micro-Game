33f257750bd69f0f2d12a5807076165cac954ba4
shivam.jha <shivam.jha@trell.in>
2022-04-06 10:13:45 +0530

    revamping code

--- UserService/struct/basicStruct.go
+++ UserService/struct/basicStruct.go
@@ -1,14 +1,13 @@
 package structs
 
 type UserDetails struct {
-	UserId         int                      `json:"userId" binding:"required"`
-	FirstName      string                   `json:"firstName" binding:"required"`
-	LastName       string                   `json:"lastName"`
-	PenName        string                   `json:"penName" binding:"required"`
-	UserEmail      string                   `json:"userEmail" binding:"required"`
-	Bio            string                   `json:"bio"`
-	Number         string                   `json:"number"`
-	UnlockedSeries map[string][]interface{} `json:"unlockedSeries"`
+	UserId    int    `json:"userId" binding:"required"`
+	FirstName string `json:"firstName" binding:"required"`
+	LastName  string `json:"lastName"`
+	PenName   string `json:"penName" binding:"required"`
+	UserEmail string `json:"userEmail" binding:"required"`
+	Bio       string `json:"bio"`
+	Number    string `json:"number"`
 }
 
 type ChapterDetails struct {
